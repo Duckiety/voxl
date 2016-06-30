500140be38096c2b9765142b87199fa351747941
Duckiety <hishamhakim9@gmail.com>
2016-06-30 17:11:01 +0100

    ver 1.0 beta

---
+++ Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.FormSkin1 = New Voxl.FormSkin()
+        Me.FlatButton1 = New Voxl.FlatButton()
+        Me.msg = New Voxl.FlatTextBox()
+        Me.chat = New Voxl.FlatListBox()
+        Me.FlatProgressBar1 = New Voxl.FlatProgressBar()
+        Me.FlatClose1 = New Voxl.FlatClose()
+        Me.FlatStatusBar1 = New Voxl.FlatStatusBar()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatButton1)
+        Me.FormSkin1.Controls.Add(Me.msg)
+        Me.FormSkin1.Controls.Add(Me.chat)
+        Me.FormSkin1.Controls.Add(Me.FlatProgressBar1)
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Controls.Add(Me.FlatStatusBar1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(284, 262)
+        Me.FormSkin1.TabIndex = 0
+        Me.FormSkin1.Text = "VOXL"
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton1.Location = New System.Drawing.Point(223, 56)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Rounded = False
+        Me.FlatButton1.Size = New System.Drawing.Size(49, 32)
+        Me.FlatButton1.TabIndex = 3
+        Me.FlatButton1.Text = "send"
+        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'msg
+        '
+        Me.msg.BackColor = System.Drawing.Color.Transparent
+        Me.msg.Location = New System.Drawing.Point(12, 56)
+        Me.msg.MaxLength = 32767
+        Me.msg.Multiline = False
+        Me.msg.Name = "msg"
+        Me.msg.ReadOnly = False
+        Me.msg.Size = New System.Drawing.Size(205, 29)
+        Me.msg.TabIndex = 1
+        Me.msg.Text = "enter here."
+        Me.msg.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.msg.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.msg.UseSystemPasswordChar = False
+        '
+        'chat
+        '
+        Me.chat.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.chat.items = New String() {""}
+        Me.chat.Location = New System.Drawing.Point(12, 91)
+        Me.chat.Name = "chat"
+        Me.chat.SelectedColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.chat.Size = New System.Drawing.Size(260, 126)
+        Me.chat.TabIndex = 2
+        Me.chat.Text = "FlatListBox1"
+        '
+        'FlatProgressBar1
+        '
+        Me.FlatProgressBar1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FlatProgressBar1.DarkerProgress = System.Drawing.Color.FromArgb(CType(CType(23, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(92, Byte), Integer))
+        Me.FlatProgressBar1.Location = New System.Drawing.Point(3, 198)
+        Me.FlatProgressBar1.Maximum = 100
+        Me.FlatProgressBar1.Name = "FlatProgressBar1"
+        Me.FlatProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatProgressBar1.Size = New System.Drawing.Size(278, 42)
+        Me.FlatProgressBar1.TabIndex = 2
+        Me.FlatProgressBar1.Text = "FlatProgressBar1"
+        Me.FlatProgressBar1.Value = 0
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(263, 3)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 1
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatStatusBar1
+        '
+        Me.FlatStatusBar1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatStatusBar1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.FlatStatusBar1.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatStatusBar1.ForeColor = System.Drawing.Color.White
+        Me.FlatStatusBar1.Location = New System.Drawing.Point(0, 239)
+        Me.FlatStatusBar1.Name = "FlatStatusBar1"
+        Me.FlatStatusBar1.RectColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatStatusBar1.ShowTimeDate = False
+        Me.FlatStatusBar1.Size = New System.Drawing.Size(284, 23)
+        Me.FlatStatusBar1.TabIndex = 0
+        Me.FlatStatusBar1.Text = "status: on. thanks for using VOXL."
+        Me.FlatStatusBar1.TextColor = System.Drawing.Color.Aqua
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FormSkin1 As Voxl.FormSkin
+    Friend WithEvents FlatStatusBar1 As Voxl.FlatStatusBar
+    Friend WithEvents FlatClose1 As Voxl.FlatClose
+    Friend WithEvents FlatProgressBar1 As Voxl.FlatProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents FlatButton1 As Voxl.FlatButton
+    Friend WithEvents msg As Voxl.FlatTextBox
+    Friend WithEvents chat As Voxl.FlatListBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Dim status As Integer
+    Dim nameVoxl As String = "You have not set a name yet! Tell voxl 'My name is !' and then your name for voxl to know you."
+    Private Sub FlatClose1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatClose1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        FlatProgressBar1.Value += 1
+        If FlatProgressBar1.Value = 100 Then
+            FlatProgressBar1.Value = 0
+        End If
+        If chat.SelectedItem = "" Then
+        Else
+            My.Computer.Clipboard.SetText(chat.SelectedItem)
+        End If
+
+    End Sub
+
+    Private Sub FlatButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatButton1.Click
+        chat.AddItem("You: " + msg.Text)
+        If msg.Text.Contains("yo ") Or msg.Text.Contains("hello ") Or msg.Text.Contains("hi ") Then
+            chat.AddItem("VOXL: hi!")
+        ElseIf msg.Text.Contains("my name is !") Then
+            Dim argument As String = msg.Text.Split("!"c)(1)
+            nameVoxl = argument
+            chat.AddItem("Nice to meet you, " + nameVoxl + "!")
+        ElseIf msg.Text.Contains("what is my name") Then
+            chat.AddItem("Your name is '" + nameVoxl + "'. Why didn't you know that already?")
+        ElseIf msg.Text.Contains("who are you") Then
+            chat.AddItem("...")
+        ElseIf msg.Text.Contains("version") Or msg.Text.Contains("ver") Then
+            chat.AddItem("Version: 1.0 alpha. The source code is availible on:" + vbNewLine + "github.com/duckiety/")
+        End If
+    End Sub
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Me.TopMost = True
+
+        MessageBox.Show("in order for VOXL to detect your message, put a space after your message if you have only put 1 word in. Also, if you click on  a message sent in chat, VOXL automatically copys it for you.")
+        Dim size As String = "300,300"
+
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        status += 1
+        FlatStatusBar1.Text = "status: on. Thanks for using VOXL.  " + status.ToString
+    End Sub
+End Class
